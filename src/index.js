0f989cc26e72492a624a747096a05b1320534596
jacobgonzales <jagonz@live.com>
2023-06-05 18:49:33 -0500

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  document.querySelector("form").addEventListener('submit', (e) => {
+    e.preventDefault()
+    createTodo(e.target['new-task-description'].value)
+  }) 
+
+})
+
+function createTodo(todo) {
+  let p = document.createElement('p')
+  let btn = document.createElement('button')
+  btn.addEventListener('click', deleteTodo)
+  btn.textContent = 'X'
+  p.textContent = todo + " "
+  p.appendChild(btn)
+  document.querySelector("#tasks").appendChild(p)
+};
+
+function deleteTodo(e) {
+  e.target.parentNode.remove()
+};
